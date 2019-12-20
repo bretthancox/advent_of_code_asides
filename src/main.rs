72281662de11b5dc11e3511235596781a27e7416
bretthancox <brett.hancox@gmail.com>
2019-12-20 14:17:42 -0500

    Pretecting day 6 part 2 from chaos

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-mod day_one;
-mod day_six;
+//mod day_one;
+//mod day_six;
 mod day_six_two;
+mod input_file_handler;
 
 fn main() {
-    day_one::day_one();    
-    day_six::day_six();
+    //day_one::day_one();    
+    //day_six::day_six();
+    day_six_two::six_two();
 }
