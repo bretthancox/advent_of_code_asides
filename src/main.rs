50a29a2074c7de7d03b4a65df42baa484ee685d4
Brett Hancox <BHancox@carekinesis.com>
2020-01-05 15:19:25 -0500

    Day 5 intcode completed

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 //mod day_one;
 //mod day_six;
-mod day_six_two;
+//mod day_six_two;
+mod day_five;
 mod input_file_handler;
+#[macro_use] extern crate text_io;
 
 fn main() {
     //day_one::day_one();    
     //day_six::day_six();
-    day_six_two::six_two();
+    //day_six_two::six_two();
+    day_five::day_five();
 }
